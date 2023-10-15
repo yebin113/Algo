ae9e334fe5efd150ba4ab33a85bc3592ac839735
Yebin <ybyb99@naver.com>
2023-10-16 00:13:11 +0900

    1016

---
+++ A_prac/Backjoon/두 배열의 합.py
@@ -0,0 +1,6 @@
+from sys import stdin
+T = int(stdin.readline())
+n = int(stdin.readline())
+n_arr = list(map(int,stdin.readline().split()))
+m = int(stdin.readline())
+m_arr = list(map(int,stdin.readline().split()))
