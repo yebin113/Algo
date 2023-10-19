e861e6f99e636824065c882d5a7f6163e2f11d85
ybyb99 <yebin4684@gmail.com>
2023-10-19 17:59:53 +0900

    1019

--- A_prac/Backjoon/Yet/휴게소세우기.py
+++ A_prac/Backjoon/Yet/휴게소세우기.py
@@ -2,20 +2,13 @@ N, M, L = map(int, input().split())
 where = list(map(int, input().split()))
 where.sort()
 
-max_length = where[-1] - where[0]
+max_length = L
 min_length = 1
 ans = max_length
+
 while min_length <= max_length:
     middle = (min_length + max_length) // 2
-    rest = 1
-    start = where[0]
+    rest = 0
     for i in range(N):
-        if where[i] >= middle + start:
-            start = where[i]
-            rest += 1
-    if rest >= L:
-        min_length = middle + 1
-        ans = middle
-    else:
-        max_length = middle - 1
-print(ans)
+        if where[i] > max_length:
+            rest += where[i]//max_length + (1)**
