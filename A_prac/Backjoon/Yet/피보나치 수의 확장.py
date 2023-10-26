2a846470a3e0a4fcc1b00bf9fe6628b42eb9db37
Yebin <ybyb99@naver.com>
2023-10-26 21:45:42 +0900

    1026

--- A_prac/Backjoon/Yet/피보나치 수의 확장.py
+++ A_prac/Backjoon/Yet/피보나치 수의 확장.py
@@ -1,14 +1,39 @@
-
-def fibo(n):
-    if n <= 1:
-        return n
-    return fibo(n-1)+fibo(n-2)
 N = int(input())
-dp_up = [0]*1000000
-dp_up[0] = 0
-dp_up[1] = 1
-for i in range(2,N+1):
-    dp_up[i]=dp_up[i-1]+dp_up[i-2]
+check = False
+if N == 0:
+    print(0)
+    print(0)
+else:
+    dp = [0] * 1000001
+    if N < 0:
+        N = -N
+        check = True
 
-dp_down = [0]*1000000
-print(dp_up(N))
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = -1
+        if N >= 3:
+            for i in range(3, N + 1):
+                # print(dp[i-2],dp[i-1])
+                if dp[i - 2] - dp[i - 1] < 0:
+
+                    dp[i] = - (abs(dp[i - 2] - dp[i - 1]) % 1000000000)
+                else:
+                    dp[i] = abs(dp[i - 2] - dp[i - 1]) % 1000000000
+    else:
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        if N >= 3:
+            for i in range(3, N + 1):
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000000
+    # print(dp[:N+1])
+    if dp[N] < 0:
+        print(-1)
+        print(-dp[N])
+    elif dp[N] == 0:
+        print(0)
+        print(0)
+    else:
+        print(1)
+        print(dp[N])
