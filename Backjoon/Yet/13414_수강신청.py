f7120b99637df295857f080ada07267d5b5f53e3
ybyb99 <yebin4684@gmail.com>
2023-11-24 17:12:22 +0900

    1124

--- Backjoon/Yet/13414_수강신청.py
+++ Backjoon/Yet/13414_수강신청.py
@@ -1,23 +1,14 @@
 k, n = map(int, input().split())
-s = [input() for i in range(n)]
-set_s = list(set(s))
-cnt = []
-for i in range(len(set_s)):
-    cnt.append([set_s[i], s.count(set_s[i])])
-num = 0
-
+d = {}
 for i in range(n):
-    p = 0
-    for j in range(len(set_s)):
-        if s[i] == cnt[j][0]:
-            c = cnt[j][1]
-            p = j
-            break
-    if c == 1:
-        num += 1
-        print(s[i])
-        if num == k:
-            break
-    else:
-        cnt[p][1] -= 1
+    d[input()] = i
 
+l = list(zip(d.keys(),d.values()))
+
+sorted_l = sorted(l,key=lambda x:x[1])
+
+if k > len(sorted_l):
+    k = len(sorted_l)
+for i in range(k):
+
+    print(sorted_l[i][0])
