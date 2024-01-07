fb84ec3b4ac85b10d4d46cdcebb2705b677faad9
Yebin <ybyb99@naver.com>
2024-01-08 00:22:15 +0900

    0108

--- Backjoon/Yet/30679_별가두기.py
+++ Backjoon/Yet/30679_별가두기.py
@@ -9,19 +9,18 @@ def rotate(dir):
 
 
 def dfs(i,j):
-    # print(f'시작 위치 {i,j}')
-    path = []
+
+    visited = [[5]*M for _ in range(N)]
+
     # 맨 처음 오른쪽
     d = 0
     ni, nj = i, j
     while 0 <= ni < N and 0 <= nj < M:
-        if visited[ni][nj] != d and (ni,nj,d) not in path:
-            path.append((ni,nj,d))
-        else:
+
+        if visited[ni][nj] == d:
             answer.append(i+1)
-            for ri,rj,dd in path:
-                visited[ri][rj] = dd
             return
+        visited[ni][nj] = d
 
         k = arr[ni][nj]
         ni += dir[d][0] * k
@@ -36,9 +35,9 @@ arr = []
 for _ in range(N):
     a = list(map(int,input().split()))
     arr.append(a)
-visited = [[5]*M for _ in range(N)]
+
 answer = []
-res = []
+
 for i in range(N):
     dfs(i,0)
 
