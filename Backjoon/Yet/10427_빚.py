4df63c65e49eee15e996452f538a827b6b77674e
Yebin <ybyb99@naver.com>
2024-01-08 22:53:53 +0900

    0108

--- Backjoon/Yet/10427_빚.py
+++ Backjoon/Yet/10427_빚.py
@@ -2,9 +2,21 @@ t = int(input())
 for tc in range(t):
     N, *arr = map(int,input().split())
     arr.sort()
-    s = [0]*N
-    ori = sum(arr)
-    for i in range(N):
-        s[i] = arr[i]*(i+1)
-    print(sum(s))
 
+    res = 0
+    # S(2) ~ S(N)
+    for i in range(2,N+1):
+        s = 0
+        # 0~i 합
+        for j in range(i):
+            s += arr[j]
+        # 해당 범위중 가장 큰 값에
+        # 고른 M개를 곱한 것에서 원래 빌린돈을 빼서 차액을 구함
+        min_s = arr[i-1]*i - s
+
+        for j in range(i,N):
+            s = s + arr[j] - arr[j-i]
+            if min_s >= arr[j] * i - s:
+                min_s = arr[j] * i - s
+        res += min_s
+    print(res)
