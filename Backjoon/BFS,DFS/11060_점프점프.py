013a4ce9a5cd5aaa86cca4c1a85460d9ad7727a3
Yebin <ybyb99@naver.com>
2024-01-25 22:34:52 +0900

    0125

--- Backjoon/BFS,DFS/11060_점프점프.py
+++ Backjoon/BFS,DFS/11060_점프점프.py
@@ -1,28 +1,18 @@
-from collections import deque
-def bfs():
-    global check
-    q = deque()
-    q.append((0,0))
-    visited[0] = 0
-    while q:
-        i, cnt = q.popleft()
-
-        if i >= N-1:
-            visited[-1] = min(cnt+1,visited[-1])
-            check = True
-        else:
-            visited[i] = min(visited[i], cnt)
-            for di in range(1,arr[i]+1):
-                ni = i + di
-                q.append((ni,cnt+1))
-
-
 N = int(input())
-arr = list(map(int,input().split()))
-check = False
-visited = [1001]*N
-bfs()
-if check == False:
-    print(-1)
+arr = list(map(int, input().split()))
+dp = [1001]*N
+dp[0] = 0
+# 0부터 N까지
+for i in range(N):
+    # 해당 위치 (+1~ 해당 위치 숫자)
+    for di in range(1, arr[i]+1):
+        ni = i + di
+        #  범위 밖으로 나가면 나가기
+        if ni >= N:
+            break
+        # 최소 갱신
+        dp[ni] = min(dp[ni], dp[i]+1)
+if dp[-1] != 1001:
+    print(dp[-1])
 else:
-    print(visited[-1]-1)
+    print(-1)
