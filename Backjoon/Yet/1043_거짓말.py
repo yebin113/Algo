6fc8059fed37b001ba2588ef645e6c799b29f941
Yebin <ybyb99@naver.com>
2024-02-20 00:35:06 +0900

    0220

--- Backjoon/Yet/1043_거짓말.py
+++ Backjoon/Yet/1043_거짓말.py
@@ -1,19 +1,63 @@
-from itertools import product
+import sys
 
-N, M = map(int, input().split())
-truth, *know_truth = map(int, input().split())
-for i in range(truth):
-    know_truth[i] -= 1
-cnt = 0
-party = [[0] * N for _ in range(M)]
+input = sys.stdin.readline
 
-# 파티에 오는 사람 기록
-for i in range(M):
-    come, *come_num = map(int, input().split())
-    for j in range(come):
-        person = come_num[j] - 1
-        party[i][person] = 1
-visit = []
-for t_or_f in product(['T', 'F'], repeat=M):
-    for i in range(M):
 
+def find(parent, x):
+    if x != parent[x]:
+        parent[x] = find(parent, parent[x])
+
+    return parent[x]
+
+
+def union(parent, a, b, know_truth):
+    a = find(parent, a)
+    b = find(parent, b)
+    # 파티에 같이 참석하는 사람들중에 진실을 알고있는 사람 존재한다면
+    # 부모노드를 그사람으로 함
+    if a in know_truth and b in know_truth:
+        return
+
+    if a in know_truth:
+        parent[b] = a
+
+    elif b in know_truth:
+        parent[a] = b
+
+    else:
+        if a < b:
+            parent[b] = a
+
+        else:
+            parent[a] = b
+
+
+n, m = map(int, input().split())
+know_truth = list(map(int, input().split()))[1:]
+
+# 파티 정보기록
+parties = []
+# union find 사용할 부모노드
+parent = list(range(n + 1))
+
+for _ in range(m):
+    # 오는 사람 기록
+    people_num, *people_come = map(int, input().split())
+
+    for i in range(people_num - 1):
+
+        union(parent, people_come[i], people_come[i + 1], know_truth)
+
+    parties.append(people_come)
+
+ans = 0
+for party in parties:
+    for i in range(len(party)):
+        # 파티중에 진실을 알고 있는 사람이 있다면, 거짓말을 못한다
+        if find(parent, party[i]) in know_truth:
+            break
+
+    else:
+        ans += 1
+
+print(ans)
