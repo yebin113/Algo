d6089be5e45e3110f31268af5d752b8d22b59cf1
yebin <yebin4684@gmail.com>
2024-02-19 16:55:06 +0900

    0219

--- Backjoon/Yet/1043_거짓말.py
+++ Backjoon/Yet/1043_거짓말.py
@@ -1,20 +1,19 @@
-from itertools import combinations_with_replacement
-N,M = map(int,input().split())
-truth, *know_truth = map(int,input().split())
+from itertools import product
+
+N, M = map(int, input().split())
+truth, *know_truth = map(int, input().split())
 for i in range(truth):
     know_truth[i] -= 1
 cnt = 0
-party = [[0]*N for _ in range(M)]
+party = [[0] * N for _ in range(M)]
 
 # 파티에 오는 사람 기록
 for i in range(M):
-    come, *come_num = map(int,input().split())
+    come, *come_num = map(int, input().split())
     for j in range(come):
         person = come_num[j] - 1
-        if person in know_truth:
-            party[i][person] = 1
-        else:
-            party[i][person] = 2
+        party[i][person] = 1
+visit = []
+for t_or_f in product(['T', 'F'], repeat=M):
+    for i in range(M):
 
-for t_or_f in combinations_with_replacement(['T','F'],M):
-    print(t_or_f)
