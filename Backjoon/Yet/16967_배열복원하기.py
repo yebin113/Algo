3e90407bcf7c46085ac73af9c326c3bbe02c0bd1
Yebin <ybyb99@naver.com>
2024-02-20 22:34:28 +0900

    0220

--- Backjoon/Yet/16967_배열복원하기.py
+++ Backjoon/Yet/16967_배열복원하기.py
@@ -6,22 +6,11 @@ for _ in range(H + X):
 A = [[0] * W for _ in range(H)]
 for i in range(H):
     for j in range(W):
-        # 왼쪽, 위
-        if 0 <= i < X or 0 <= j < Y:
-            print(i,j,'왼쪽위')
-            A[i][j] = B[i][j]
-        # 오른쪽, 아래
-        elif H-X <= i < H or W-Y <= j < W:
-            print(i, j, '오른쪽 아래')
-            A[i][j] = B[i+X][j+Y]
-        # 중간
-        elif Y <= j < 2 * Y or X <= i < 2 * X:
-            print(i,j, Y, 2*Y, X, 2*X)
+        A[i][j] = B[i][j]
 
-            A[i][j] = B[i][j] - B[i-X][j-Y]
-        # else:
-        #     A[i][j] = B[i + X][j + Y] - B[i][j]
-
-    for k in range(H):
-        print(*A[k])
-    print()
+for i in range(X, H):
+    for j in range(Y, W):
+        A[i][j] = B[i][j] - A[i - X][j - Y]
+for k in range(H):
+    print(*A[k])
+# print()
--- Backjoon/Yet/HELPrandom.py
+++ Backjoon/Yet/HELPrandom.py
@@ -1,10 +1,10 @@
-import sys
-
-path = 'random_num.txt'
-sys.stdout = open(path, 'w')
-
-import random
-import string
+# import sys
+#
+# path = 'random_num.txt'
+# sys.stdout = open(path, 'w')
+#
+# import random
+# import string
 
 # 문자열
 # def generate_random_string(length):
@@ -16,8 +16,20 @@ import string
 # desired_length = 1000  # 원하는 문자열 길이
 # random_string = generate_random_string(desired_length)
 # print(random_string)
-print(1000)
-# 숫자
-for i in range(1000):
-    a = random.randint(0,100)
-    print(a,end=' ')
+# print(1000)
+# # 숫자
+# for i in range(1000):
+#     a = random.randint(0,100)
+#     print(a,end=' ')
+
+a = [[1,2,1,9,7,2],[16,22,11,91,72,25],[18,92,12,1,7,3],[12,29,11,90,7,21]]
+b = [[0]*8 for _ in range(5)]
+for i in range(len(a)):
+    for j in range(len(a[0])):
+        b[i][j] += a[i][j]
+        b[i+1][j+2] += a[i][j]
+for i in range(len(a)):
+    print(*a[i])
+print()
+for i in range(len(b)):
+    print(*b[i])
