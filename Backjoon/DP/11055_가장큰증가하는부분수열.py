ce03a13bdc2622ecd34eb1e7285a81e87b5c8db0
Yebin <ybyb99@naver.com>
2024-03-26 00:26:52 +0900

    0326

--- Backjoon/DP/11055_가장큰증가하는부분수열.py
+++ Backjoon/DP/11055_가장큰증가하는부분수열.py
@@ -1,2 +1,15 @@
 N = int(input())
-numbers = list(map(int,input().split()))
+numbers = list(map(int, input().split()))
+
+dp = [1] * N
+dp[0] = numbers[0]
+for i in range(1, N):
+    for j in range(i):
+        # 증가하면 현재 수 + 지금 수 와 지금 값 갱신
+        if numbers[j] < numbers[i]:
+            dp[i] = max(dp[i], dp[j] + numbers[i])
+        # 증가 안하면 이전 수와 현재값 갱신
+        else:
+            dp[i] = max(dp[i], numbers[i])
+
+print(max(dp))
