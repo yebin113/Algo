4d8332a41d4590576554fbd4b5e9691d05be58c0
Yebin <ybyb99@naver.com>
2024-03-29 00:59:14 +0900

    0329

--- Backjoon/DP/2015_수들의합4.py
+++ Backjoon/DP/2015_수들의합4.py
@@ -1,31 +1,18 @@
-N,M = map(int,input().split())
-arr = list(map(int,input().split()))
+N, K = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
 
-arr.sort()
-cnt = 0
+dp = [0]
 
-start = 0
-end = 1
-while start < N and end < N:
-    hap = arr[end] + arr[start]
-    # print("start",start, "arr[start]",arr[start],"end",end, "arr[end]",arr[end],"hap",hap)
-    # 합이 M일때 답 + 1
-    if hap == M:
-        cnt += 1
-        end += 1
-        if end == N:
-            start += 1
-            end = start + 1
-    elif hap < M:
-        end += 1
-
-        if end == N:
-            start += 1
-            end = start + 1
-
-    else:
-        start += 1
-        end = start + 1
-        continue
-
-print(cnt)
+# 누적합
+for i in range(1, N + 1):
+    dp.append(dp[i - 1] + arr[i])
+# 딕셔너리 저장
+num_dict = {}
+answer = 0
+# 배열을 돌아가면서
+for i in range(N + 1):
+    # dp[i] - K 값이 num_dict에 존재하는 경우 answer에 더함
+    answer += num_dict.get(dp[i] - K, 0)
+    # dp[i] 값을 num_dict에 저장
+    num_dict[dp[i]] = num_dict.get(dp[i], 0) + 1
+print(answer)
